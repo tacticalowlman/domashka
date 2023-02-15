1dc767204e402fe1b2d6677646f4027cd2aac7dd
tacticalowlman <43387931+tacticalowlman@users.noreply.github.com>
2023-02-15 17:55:26 +0300

    Create regular.py

---
+++ regular.py
@@ -0,0 +1,4 @@
+import re
+
+minyons_1 = "Minyon minYon Mineyon mIniYon monyon minyoN mInYoN miNYon maNYoN MINYON mOnYoN ManyoN"
+minyons_2 = "minyon miNYon mIneYon MinYoN MaNyon MonYon Minyon Bob ManYon minYON MiNyon MinyoN Monyo
